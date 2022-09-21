8623f28e4467fdebc07de4fd2051ddde05fcb18f
Diego Ramos <diegorramos84@gmail.com>
2022-09-21 19:31:23 +0100

    add projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { CodeBracketIcon } from "@heroicons/react/24/solid";
+import { projects } from "../data";
+
 
 const Projects = () => {
   return (
     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
-          <CodeIcon className="mx-auto inline-block w-10 mb-4" />
+          <CodeBracketIcon className="mx-auto inline-block w-10 mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
             Apps I've Built
           </h1>
---
+++ src/data.js
@@ -0,0 +1,25 @@
+export const projects = [
+  {
+    title: "Plant Swap",
+    subtitle: "Placeholder",
+    description: "lorem",
+    image: "./plantswap.png",
+    link: "https://www.plantswap.live/"
+
+  },
+  {
+    title: "Con[text]",
+    subtitle: "Placeholder",
+    description: "lorem",
+    image: "./context.gif",
+    link: "https://team50.herokuapp.com/"
+
+  },
+  {
+    title: "Rent-a-Cat",
+    subtitle: "Placeholder",
+    description: "lorem",
+    image: "./context.gif",
+    link: "https://rent-a-cat-batch901.herokuapp.com/"
+  },
+]
