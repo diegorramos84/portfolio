054cef3febc83e7dd78505a34c0fe25ffa4646a4
Diego Ramos <diegorramos84@gmail.com>
2022-09-22 15:50:35 +0100

    add skills and tech stack icons

--- src/data.js
+++ src/data.js
@@ -1,3 +1,7 @@
+import IconLanguageRuby from "./components/icons/IconLanguageRuby.tsx"
+import { FaJsSquare, FaLinux, FaNodeJs, FaReact } from "react-icons/fa"
+import { SiRubyonrails } from "react-icons/si"
+
 export const projects = [
   {
     title: "Plant Swap",
@@ -21,5 +25,32 @@ export const projects = [
     description: "lorem",
     image: "./cats.png",
     link: "https://rent-a-cat-batch901.herokuapp.com/"
+  },
+]
+
+export const skills = [
+  {
+    name: "Ruby",
+    icon: <IconLanguageRuby className="mr-2 text-red-700" />
+  },
+  {
+    name: "Rails",
+    icon: <SiRubyonrails className="mr-2 bg-red-700 text-white rounded-full" />
+  },
+  {
+    name: "Javascript",
+    icon: <FaJsSquare className="mr-2 text-yellow-400" />,
+  },
+  {
+    name: "Linux",
+    icon: <FaLinux className="mr-2 text-black" />,
+  },
+  {
+    name: "React",
+    icon: <FaReact className="mr-2 text-blue-300" />,
+  },
+  {
+    name: "Node",
+    icon: <FaNodeJs className="mr-2 text-green-300" />,
   },
 ]
