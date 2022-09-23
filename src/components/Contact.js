58ea032756f32b1e6839af3e26c04ea60dea8ace
Diego Ramos <diegorramos84@gmail.com>
2022-09-23 14:34:16 +0100

    add netlify form API

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,37 @@
 import React from "react";
+import { useState } from "react"
 
 const Contact = () => {
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
+
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&")
+  }
+
+  const handleSubmit= (e) => {
+    e.preventDefault()
+    fetch("/", {
+      method: "POST",
+      heades: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, message })
+    })
+    .then(() => alert("Message sent!"))
+    .catch((error) => alert(error))
+  }
+
   return (
     <section id="contact" className="relative">
       <form
         netlify
         name="contact"
+        onSubmit={ handleSubmit }
         className="flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
         <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
           Hire Me
@@ -23,6 +49,7 @@ const Contact = () => {
             id="name"
             name="name"
             className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            onChange={ (e) => setName(e.target.value) }
           />
         </div>
         <div className="relative mb-4">
@@ -34,6 +61,7 @@ const Contact = () => {
             id="email"
             name="email"
             className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            onChange={ (e) => setEmail(e.target.value) }
           />
         </div>
         <div className="relative mb-4">
@@ -46,6 +74,7 @@ const Contact = () => {
             id="message"
             name="message"
             className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+            onChange={ (e) => setMessage(e.target.value) }
           />
         </div>
         <button
