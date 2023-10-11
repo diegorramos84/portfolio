a1e9720df353178b3288c96ce6ee7e7bb297215e
Diego Ramos <diegorramos84@gmail.com>
2023-10-11 10:55:19 +0100

    change how many projects per row

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,20 +13,18 @@ const Projects = () => {
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
             Apps I've Built
           </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-
-          </p>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base"></p>
         </div>
         <div className="flex flex-col sm:flex-row flex-wrap -m-4 ">
           {projects.map((project) => (
             <a
               href={project.link}
               key={project.image}
-              className="sm:w-1/2 w-100 h-80 p-4">
-              <div className="flex relative h-full">
+              className="sm:w-1/3 w-100 h-80 p-4">
+              <div className="flex relative w-100 h-full">
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-scale-down lg:object-cover object-center"
+                  className="absolute inset-0 w-full h-full object-scale-down lg:object-fill object-center"
                   src={project.image}
                 />
                 <div className="px-8 py-10 relative z-10 w-full h-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100 flex-col items-center justify-center">
