b7eac9569c21d5ef4fc28fe178a42e7bbbee5138
Diego Ramos <diegorramos84@gmail.com>
2023-10-11 10:30:14 +0100

    fix social bg, fix image scaling, fix github icon position

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -26,18 +26,18 @@ const Projects = () => {
               <div className="flex relative h-full">
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-fit object-center"
+                  className="absolute inset-0 w-full h-full object-scale-down lg:object-cover object-center"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full h-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                <div className="px-8 py-10 relative z-10 w-full h-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100 flex-col items-center justify-center">
                   <h2 className="tracking-widest text-lg title-font font-medium text-red-666 mb-1">
                     {project.subtitle}
                   </h2>
                   <h1 className="title-font text-lg font-medium text-white mb-3">
                     {project.title}
                   </h1>
-                  <p className="leading-relaxed">{project.description}</p>
-                  <a type="button" rel="noreferrer" href={project.github} target="_blank" className="text-xl text-white pt-1 bg-gray-900"><BsGithub /></a>
+                  <p className="leading-relaxed mb-2">{project.description}</p>
+                  <a type="button" rel="noreferrer" href={project.github} target="_blank" className="text-xl text-white pt-1 bg-gray-900 flex items-center justify-center"><BsGithub /></a>
                 </div>
               </div>
             </a>
--- src/components/Social.js
+++ src/components/Social.js
@@ -6,13 +6,13 @@ const Social = () => {
   return(
     <div className="icon-bar fixed botton-0 z-20">
     <div className="twitter">
-      <a type="button" rel="noreferrer" href="https://twitter.com/DIERRamos84" target="_blank" className="text-xl bg-sky-500 text-white p-2"><BsTwitter /></a>
+      <a type="button" rel="noreferrer" href="https://twitter.com/DIERRamos84" target="_blank" className="text-xl text-white p-2"><BsTwitter /></a>
     </div>
     <div className="linkedin">
-      <a type="button" rel="noreferrer" href="https://www.linkedin.com/in/diegorramos84" target="_blank" className="text-xl text-white p-2 bg-[#0077B5]"><BsLinkedin /></a>
+      <a type="button" rel="noreferrer" href="https://www.linkedin.com/in/diegorramos84" target="_blank" className="text-xl text-white p-2 "><BsLinkedin /></a>
     </div>
     <div className="github">
-      <a type="button" rel="noreferrer"href="https://github.com/diegorramos84" target="_blank" className="text-xl text-white p-2 bg-[#171515]"><BsGithub /></a>
+      <a type="button" rel="noreferrer"href="https://github.com/diegorramos84" target="_blank" className="text-xl text-white p-2"><BsGithub /></a>
     </div>
     </div>
   )
